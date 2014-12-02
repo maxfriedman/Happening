986d63f75f843f56c1bf8f594b065f723f8e279d
Max Friedman <maxfriedman422@gmail.com>
2014-12-02 04:10:09 -0500

    Moved Files

---
+++ HappeningParse/showMyEventVC.h
@@ -0,0 +1,17 @@
+//
+//  showMyEventVC.h
+//  HappeningParse
+//
+//  Created by Max on 12/2/14.
+//  Copyright (c) 2014 Happening. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface showMyEventVC : UIViewController
+
+@property NSString *eventID;
+
+@property (strong, nonatomic) IBOutlet UILabel *eventIDLabel;
+
+@end
